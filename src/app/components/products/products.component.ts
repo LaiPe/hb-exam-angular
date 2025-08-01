3d4fcdc9073671d17aaf1b05cc6b46c023fc3228
Léo Peyronnet <peyronnet.leo@gmail.com>
2025-08-01 11:30:14 +0200

    feat : page produits

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Produit } from '../../models/produit';
+import { Subscription } from 'rxjs';
+import { ProduitsService } from '../../services/produits/produits.service';
+import { ProductCardComponent } from "../product-card/product-card.component";
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+  products : Array<Produit> = [];
+  loading : boolean = true;
+  private subscription!: Subscription;
 
+  service = inject(ProduitsService);
+
+  ngOnInit() {
+    this.loadProduits();
+  }
+
+  loadProduits(){
+    this.subscription = this.service.getAll().subscribe({
+      next : products => {
+        this.products = products;
+        this.loading = false;
+      },
+      error : error => {
+        console.log(error);
+        this.loading = false;
+      }
+    });
+  }
 }
